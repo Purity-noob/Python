97be880a46a2bd383db6a8b9fb8832a4f354eae7
Purity Njenga <serenmalika09@gmail.com>
2024-02-21 18:13:48 +0300

    User input

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("Enter your name:")
+age = (input("Enter your age:"))
+location =input("Enter your location:")
+print("Hello " + name + " at your age which is " + age + " and location, " + location + " ,you are not viable for the position" )
